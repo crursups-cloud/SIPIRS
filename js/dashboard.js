a8c74427a35b3ff882b2c1f7e7f006f7d3fff821
crursups-cloud <cru.rsups@gmail.com>
2026-07-28 14:51:24 +0700

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,21 @@
+const user = JSON.parse(localStorage.getItem("sipirsUser"));
 
+if(!user){
+
+    window.location.href="login.html";
+
+}
+
+document.getElementById("welcome").innerHTML =
+"Selamat Datang, " + user.nama + " 👋";
+
+document.getElementById("userName").innerHTML =
+user.nama;
+
+function logout(){
+
+    localStorage.removeItem("sipirsUser");
+
+    window.location.href="../index.html";
+
+}
