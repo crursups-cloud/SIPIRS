1ce454dd5ef1764ffa648cc53c013446934a2ebb
crursups-cloud <cru.rsups@gmail.com>
2026-07-29 13:42:04 +0700

    Create publication.js

---
+++ js/publication.js
@@ -0,0 +1,9 @@
+// ==========================================
+// SIPIRS - PUBLICATION
+// ==========================================
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    console.log("Publication page berhasil dimuat.");
+
+});
