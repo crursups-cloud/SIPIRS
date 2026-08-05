6ce11e03fa2b65d3de15cc994b405281b4b52876
crursups-cloud <cru.rsups@gmail.com>
2026-08-05 14:02:24 +0700

    Update register.js

--- js/register.js
+++ js/register.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", async function(){
 
+    loadMasterRegister();
+
+});
+
+async function loadMasterRegister(){
+
+    // nanti kita isi setelah Apps Script selesai
+
+}
