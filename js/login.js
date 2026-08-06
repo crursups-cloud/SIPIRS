29d51e1832d33e3d6e46ae2d35c364cec722b92c
crursups-cloud <cru.rsups@gmail.com>
2026-08-06 14:39:00 +0700

    Update login.js

--- js/login.js
+++ js/login.js
@@ -46,3 +46,35 @@ async function loginUser(e){
     }
 
 }
+function togglePassword(){
+
+    const input =
+    document.getElementById("password");
+
+    const icon =
+    document.querySelector("#togglePassword i");
+
+    if(input.type==="password"){
+
+        input.type="text";
+
+        icon.classList.remove("bi-eye");
+        icon.classList.add("bi-eye-slash");
+
+    }else{
+
+        input.type="password";
+
+        icon.classList.remove("bi-eye-slash");
+        icon.classList.add("bi-eye");
+
+    }
+
+}
+
+document
+.getElementById("togglePassword")
+.addEventListener(
+    "click",
+    togglePassword
+);
